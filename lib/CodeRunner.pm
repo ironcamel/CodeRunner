0be0e2366bf2967ee5c85f056419175ed9ca5085
Naveed Massjouni <naveedm9@gmail.com>
2011-11-04 18:58:38 +0000

    Creating /problems/:problem route. Problems are parsed from yamel file in ./problems folder.

--- lib/CodeRunner.pm
+++ lib/CodeRunner.pm
@@ -4,36 +4,48 @@ use v5.10;
 use Dancer ':syntax';
 use Dancer::Plugin::Cache::CHI;
 use Dancer::Plugin::Stomp;
+use File::Basename qw(fileparse);
 use YAML qw(LoadFile);
 
 get '/' => sub {
-    my $problem = LoadFile(config->{appdir} . '/problems/penney.yml');
-    template index => {
-        problem => $problem,
+    my @problems;
+    for my $path (glob config->{appdir} . '/problems/*.yml') {
+        my $problem = LoadFile($path);
+        my($name, $dir, $suffix) = fileparse($path, '.yml', '.yaml');
+        push @problems,
+            { title => $problem->{title}, url => uri_for("/problems/$name") };
+    }
+    template problems => {
+        problems => \@problems,
     };
 };
 
-post '/' => sub {
+get '/problems/:problem' => sub {
+    template problem => {
+        problem => get_problem(param 'problem'),
+    };
+};
+
+post '/problems/:problem' => sub {
+    my $problem = get_problem(param 'problem');
     my $file = upload 'code_file';
-    #my $problem = param 'problem';
-    my $problem = 'penney';
     my $run_id = session->id;
     cache_set $run_id => to_json({status => -1});
     my $msg = to_json({
         run_id   => $run_id,
         code     => $file->content,
-        filename => $file->basename,
         language => param('language') || guess_lang($file->basename),
-        problem  => config->{problems}{$problem},
+        problem  => $problem,
         cb_url   => uri_for('/cb')->as_string,
-    }, { pretty => 1 });
+    }, { utf8 => 1 });
     stomp->send({
         destination => config->{queue},
         body        => $msg,
     });
-    template index => {
+    template problem => {
         is_running => 1,
-        run_id => $run_id,
+        run_id   => $run_id,
+        problem  => $problem,
     };
 };
 
@@ -48,6 +60,11 @@ post '/cb' => sub {
     cache_set $run_id => request->body;
     return '';
 };
+
+sub get_problem {
+    my ($prob_name) = @_;
+    return LoadFile(config->{appdir} . "/problems/$prob_name.yml");
+}
 
 sub guess_lang {
     my ($filename) = @_;
