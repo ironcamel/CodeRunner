54d86c1197ba1a6288a0b72bd1789b033cf12869
Naveed Massjouni <naveedm9@gmail.com>
2011-11-12 06:25:26 +0000

    Using timeout from IPC::Run

--- bin/worker.pl
+++ bin/worker.pl
@@ -2,39 +2,37 @@
 use strict;
 use warnings;
 use v5.10;
-use Data::Dump qw(dd dump);
+use Data::Dump qw(dd);
 use File::Temp;
 use FindBin qw($RealBin);
-use IPC::Run qw(run);
+use IPC::Run qw(run timeout);
 use JSON qw(decode_json encode_json);
 use LWP::UserAgent;
 use Net::Stomp;
 use Try::Tiny;
-use Unix::PID::Tiny;
 use YAML qw(LoadFile);
 
-my $config = LoadFile("$RealBin/../config.yml");
-my $agent = LWP::UserAgent->new();
+my $CONFIG = LoadFile("$RealBin/../config.yml");
+my $AGENT = LWP::UserAgent->new();
 
 my $stomp = Net::Stomp->new({
-    hostname => $config->{plugins}{Stomp}{default}{hostname},
-    port     => $config->{plugins}{Stomp}{default}{port},
+    hostname => $CONFIG->{plugins}{Stomp}{default}{hostname},
+    port     => $CONFIG->{plugins}{Stomp}{default}{port},
 });
 $stomp->connect();
 $stomp->subscribe({
-    destination => $config->{queue},
+    destination => $CONFIG->{queue},
     ack         => 'client',
 });
 
 while (1) {
     my $frame = $stomp->receive_frame;
     my $msg = $frame->body;
-    print "processing $msg\n";
+    say "processing...";
+    #say "processing $msg";
     try {
         my $data = decode_json($msg);
-        my $result = validate($data, $agent);
-        #$agent->post($data->{cb_url}, content_type => 'application/json',
-            #Content => encode_json($result));
+        validate($data);
     } catch {
         say "failed: $_";
     };
@@ -45,28 +43,35 @@ $stomp->disconnect;
 # functions -------------------------------------------------------------------
 
 sub validate {
-    my ($data, $agent) = @_;
+    my ($data) = @_;
     my $problem = $data->{problem};
     my $input = $problem->{input};
+    my $reason = "I dont know.";
     say "going to run code ...";
-    my $pid = fork;
-    if ($pid == 0) { # this is the child process
-        my $tmp = File::Temp->new();
-        #print $tmp $data->{code};
-        run [ '/usr/local/bin/perl', "$tmp" ], \$input, \my $out;
-        $out ||= '';
-        my $status = $out eq $problem->{output} ? 1 : 0;
-        my $result = {
-            status => $status,
-            reason => "I dont know",
-            run_id => $data->{run_id},
-        };
-        $agent->post($data->{cb_url}, content_type => 'application/json',
-            Content => encode_json($result));
-        exit;
+    my $tmp = File::Temp->new();
+    print $tmp $data->{code};
+    my ($out, $err);
+    try {
+        run [ '/usr/local/bin/perl', "$tmp" ],
+            \$input, \$out, \$err, timeout(3);
+    } catch {
+        post_result(0, 'Took too long.', $data);
     }
-    #sleep 6;
-    #dd $result;
-    #return $result;
+    $out ||= '';
+    my $status = $out eq $problem->{output} ? 1 : 0;
+    $reason = "Wrong answer." unless $status;
+    post_result($status, $reason, $data);
 }
 
+sub post_result {
+    my ($status, $reason, $data) = @_;
+    print "posting result:";
+    my $result = {
+        status => $status,
+        reason => $reason,
+        run_id => $data->{run_id},
+    };
+    dd $result;
+    $AGENT->post($data->{cb_url}, content_type => 'application/json',
+        Content => encode_json($result));
+}
