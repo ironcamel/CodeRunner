8f67f55864ca7e2ecdad6b23f1aff6d3520bbf0e
Naveed Massjouni <naveedm9@gmail.com>
2013-02-21 11:00:33 +0000

    Added support for env_bundle_url and compile_cmd

--- bin/worker.pl
+++ bin/worker.pl
@@ -19,6 +19,8 @@ my $AGENT = LWP::UserAgent->new(ssl_opts => { verify_hostname => 0 });
 my $log_path = "/tmp/coderunner-worker.log";
 my $LOG = IO::File->new($log_path, 'a')
     or die "Could not open $log_path : $!\n";
+$LOG->autoflush(1);
+STDOUT->autoflush(1);
 
 my $stomp = Net::Stomp->new({
     hostname => $CONFIG->{plugins}{Stomp}{default}{hostname},
@@ -36,6 +38,9 @@ $SIG{INT} = sub {
 $SIG{PIPE} = 'IGNORE';
 
 say "worker is starting";
+
+# main loop --------------------------------------------------------------------
+
 while (1) {
     my $frame = $stomp->receive_frame;
     my $msg = $frame->body;
@@ -43,7 +48,7 @@ while (1) {
     say "processing msg ...";
     try {
         $data = decode_json($msg);
-        validate($data);
+        process_msg($data);
     } catch {
         debug("failed to process msg: $_");
         post_result(0, $_, $data);
@@ -51,29 +56,25 @@ while (1) {
         $stomp->ack({ frame => $frame });
     };
 }
-$stomp->disconnect;
 
 # functions -------------------------------------------------------------------
 
-sub validate {
+sub process_msg {
     my ($data) = @_;
     log_msg($data);
-    my $lang = $data->{language};
-    my $problem = $data->{problem};
-    my $out = run_code(
-        $lang, $data->{code}, $data->{file_name}, $problem->{input});
-    my $status = $out eq $problem->{output} ? 1 : 0;
+    my $out = run_code($data);
+    my $status = $out eq $data->{problem}{output} ? 1 : 0;
     my $reason = $status == 1 ? 'Success' : 'Wrong answer';
     post_result($status, $reason, $data);
 }
 
 sub run_code {
-    my ($lang, $code, $file_name, $input) = @_;
+    my ($data) = @_;
 
     my $pid = open my $child_process, '-|';
     if (!$pid) { # child process starts here
         try {
-            run_in_chroot($lang, $code, $file_name, $input);
+            run_in_chroot($data);
         } catch {
             say "ERROR: $_";
         };
@@ -85,6 +86,7 @@ sub run_code {
     close $child_process;
     my $result = join '', @out;
     debug('output: ' . substr $result, 0, 100);
+    debug("full: $result");
 
     # Lets clean up after ourselves.
     #$cmd = "rm -rf $jail";
@@ -95,43 +97,66 @@ sub run_code {
 }
 
 sub run_in_chroot {
-    my ($lang, $code, $file_name, $input) = @_;
+    my ($data) = @_;
+    my $lang      = $data->{language};
+    my $code      = $data->{code};
+    my $file_name = $data->{file_name};
+    my $problem   = $data->{problem};
+    my $input     = $problem->{input};
 
     debug("Setting up chroot ...");
     my $jail = '/var/chroot1';
+    #$jail = '/var/chroot-arch';
     my $cmd = "rm -rf $jail";
-    debug($cmd);
-    system $cmd;
+    #debug($cmd);
+    #system $cmd;
 
     $cmd = "tar -xf $jail.tar -C /var";
-    debug($cmd);
-    die "Could not build chroot jail: $!" unless system($cmd) == 0;
+    #debug($cmd);
+    #die "Could not build chroot jail: $!" unless system($cmd) == 0;
 
     chdir $jail or die "Failed to chdir into $jail: $!";
+
+    $cmd = "mount -t proc proc proc/";
+    #debug($cmd);
+    #die "Could not mount /proc: $!" unless system($cmd) == 0;
 
     debug("chroot $jail");
     chroot $jail or die "could not chroot: $!";
 
     # drop root privileges
     my $new_uid = getpwnam('coderunner');
-    unless (defined $new_uid) {
-        say "Can't find uid for 'coderunner'";
-        exit 1;
-    }
-    $< = $> = $new_uid;
-    setgid($new_uid);
+    die "Can't find uid for 'coderunner'" unless defined $new_uid;
+    $< = $> = $new_uid; # Update the real and effective user id
+    setgid($new_uid);   # Update the group id
 
     my ($out, $err, @cmd) = ('', '');
     my $tmpdir = File::Temp->newdir();
     my $tmpfile = File::Temp->new();
     given ($lang) {
         when ('java') {
+            chdir "$tmpdir";
+            if (my $url = $data->{env_bundle_url}) {
+                #$url = 'http://ironcamel.com/files/penny.tar';
+                debug("getting bundle from $url");
+                my $bundle_path = "bundle.tar";
+                # TODO: install LWP::Protocol::https, libssl-dev,
+                # libnet-ssleay-perl in chroot
+                my $res = $AGENT->mirror($url, $bundle_path);
+                debug($res->status_line);
+                die $res->status_line unless $res->is_success;
+                run ["tar xf $bundle_path"] == 0
+                    or die "Could not expand bundle $bundle_path $!";
+            }
             my $class_name = (split /\./, $file_name)[0];
             my $path = "$tmpdir/$file_name";
             open my $java_file, '>', $path;
             print $java_file $code;
-            run([ 'javac', "$path" ]) or die "Failed to compile java code";
-            @cmd = ($lang, -classpath => "$tmpdir", $class_name);
+            my $compile_cmd = $data->{compile_cmd} || "javac $path";
+            system($compile_cmd) == 0 or
+                die "Failed to compile java code via [$compile_cmd]: $!";
+            #@cmd = ($lang, -classpath => "$tmpdir", $class_name);
+            @cmd = ($lang, $class_name);
         }
         when ('c++') {
             my $path = "$tmpdir/foo.cpp";
@@ -141,7 +166,7 @@ sub run_in_chroot {
                 or die "Failed to compile c/c++ code";
             @cmd = ("$tmpdir/foo");
         }
-        when ([qw(perl python java)]) {
+        when ([qw(perl python ruby)]) {
             print $tmpfile $code;
             @cmd =($lang, "$tmpfile");
         }
@@ -154,8 +179,9 @@ sub run_in_chroot {
     try {
         run(\@cmd, \$input, \$out, \$err, timeout(3));
     } catch {
-        print "Took too long\n";
+        print "Took too long $_\n";
     };
+    chdir '/'; # Leave the temp dir so it can be auto deleted
     print $out;
 }
 
@@ -181,7 +207,6 @@ sub post_result {
 sub debug {
     print $LOG '[' . localtime . '] (DEBUG) ';
     say $LOG dump(@_);
-    $LOG->flush;
 }
 
 sub log_msg {
